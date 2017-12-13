bd7e82ef6e8e47cc446c9a476ae18a39fa65f8ad
isela rojo <iselarojo2017@gmail.com>
2017-12-13 12:25:18 -0600

    portafolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule
